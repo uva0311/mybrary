bcbce86ab30f64f93a78db9eb65137d11e34bd36
Arthur Lam <lam.arthur0311@gmail.com>
2019-11-23 19:15:52 +0000

    setup initial author routes

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // Setup MongoDB connection
 const mongoose = require('mongoose');
@@ -26,5 +29,6 @@ db.once('open', () => console.log('Connected to Mongoose'));
 
 // Routes
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
